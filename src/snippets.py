2bd233c6fb7ee748cf8cb5821eab2ef53d252788
mr-hoffman <hoffmant@hilbertk12.org>
2025-12-19 10:25:50 -0600

    Implement color sorting with optical sensor
    Added optical sensor functionality to sort colors based on opponent's alliance color.

--- src/snippets.py
+++ src/snippets.py
@@ -31,3 +31,27 @@ lft_motor_group.set_velocity((controller_1.axis3.position() + controller_1.axis1
 rgt_motor_group.set_velocity((controller_1.axis3.position() - controller_1.axis1.position()), PERCENT)
 lft_motor_group.spin(FORWARD)
 rgt_motor_group.spin(FORWARD)
+
+
+
+##### Optical Sensor #####
+optical_sensor = Optical(Ports.PORT10)
+
+# returns True if sensor sees anythine EXCEPT opponent alliance color
+# returns False if sensor sees opponent alliance color
+# opponent color must be passed in as a paremeter in format Color.<color_name> (see documentation)
+def color_sort(opponent_color):
+  if not optical_sensor.color() == opponent_color:
+    return True
+  else:
+    return False
+
+# to use above, call the function in your button.pressing() block:
+# for instance, to spin an intake one direction for your team color 
+# and the opposite direction for opponent's color
+if controller1.buttonA.pressing():
+  if color_sort(Color.RED):
+    selected_motor.spin(FORWARD)
+  else:
+    selected_motor.spin(REVERSE)
+
